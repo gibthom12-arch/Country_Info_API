a84719013d6a160046371fef5b42bd15e37237ba
gibthom12-arch <gibthom12@gmail.com>
2026-09-09 22:05:37 +0100

    Add popups when countries clicked
    Added functionality to fetch and display country data in a popup.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const response = await fetch("https://api.aviationstack.com/v1/flights?access_key=7db7a9d97d3f8d40b257f2e71e0faafb")
+
 Map_Maker();
 
 async function fetchData(CountryName){
@@ -7,8 +7,22 @@ async function fetchData(CountryName){
         if (!response.ok) {
             throw new Error("Could not fetch resource");
         }
+        // Get all the information you want from API and store it in variables
+        // Pass variables into openPopup
+        // Make spaces for variables to fill in HTML for popup
         const data = await response.json();
         console.log(data);
+        const fullName = data.name;
+        const NativeName = data.nativeName;
+        const Demonym = data.demonym;
+        const Subregion = data.subregion;
+        const population = data.population;
+        const timezone = data.timezones;
+        const tld = data.topLevelDomain;
+        const AbrevBorders = data.borders;
+        const Capital = data.capital;
+        const flag = data.flags.png;
+        openPopup(fullName, NativeName, Demonym, Subregion, population, timezone, tld, AbrevBorders, Capital, flag);
     }
     catch(error) {
         console.error(error);
@@ -17,8 +31,8 @@ async function fetchData(CountryName){
 
 function Map_Maker(){
     const bounds = [ [-180, -175], [180, 185]];
-    var map = L.map('map', { maxBounds: bounds, maxBoundsViscosity: 0.9, minZoom: 3}).setView([0, 0], -5);
-    L.tileLayer('https://api.maptiler.com/maps/hybrid-v4/{z}/{x}/{y}.jpg?key=ihEX9ouMm5tQKqtHxBFw', {
+    const map = L.map('map', { maxBounds: bounds, maxBoundsViscosity: 0.9, minZoom: 3}).setView([0, 0], -5);
+    L.tileLayer('https://api.maptiler.com/maps/hybrid-v4/{z}/{x}/{y}.jpg?key=Input_key', {
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
     }).addTo(map);
 
@@ -35,4 +49,24 @@ function Map_Maker(){
                 });
             }}).addTo(map);
         });
+}
+
+function openPopup(fullName, NativeName, Demonym, Subregion, population, timezone, tld, AbrevBorders, Capital, flag) {
+    let popup = document.getElementById("popup");
+    popup.classList.add("open-popup");
+
+    popupTitle.textContent = fullName;
+    popupNativeName.textContent = NativeName;
+    popupCapital.textContent = Capital;
+    popupdemonym.textContent = Demonym;
+    popupSubRegion.textContent = Subregion;
+    popupPopulation.textContent = population;
+    popupTimezone.textContent = timezone;
+    popupTLD.textContent = tld;
+    popupBorderingCountries.textContent = AbrevBorders;
+}
+
+function closePopup() {
+    let popup = document.getElementById("popup");
+    popup.classList.remove("open-popup")
 }
